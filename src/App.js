b227ee943a1b7ee2dedd4456457839dd7985e2aa
Hamer Patarroyo <hamer.patarroyo@gmail.com>
2021-07-12 18:35:30 -0500

    adding songs letters project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios';
+import React, { Fragment, useEffect, useState } from 'react';
+import Cancion from './components/Cancion';
+import Formulario from './components/Formulario';
+import Info from './components/Info';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [busquedaLetra, setBusquedaLetra] = useState({});
+    const [letra, setLetra] = useState('');
+    const [info, setInfo] = useState({});
+
+    useEffect(() => {
+        if (Object.keys(busquedaLetra).length === 0) {
+            return;
+        }
+
+        const consultarApiLetra = async () => {
+
+            const url = `https://api.lyrics.ovh/v1/${busquedaLetra.artista}/${busquedaLetra.cancion}`;
+            const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${busquedaLetra.artista}`;
+
+
+
+            const [letra, informacion] = await Promise.all([
+                Axios.get(url),
+                Axios.get(url2),
+            ]);
+
+            setLetra(letra.data.lyrics);
+            setInfo(informacion.data.artists[0]);
+
+            setBusquedaLetra({});
+        }
+
+        consultarApiLetra();
+    }, [busquedaLetra, info])
+
+    return (
+        <Fragment>
+            <Formulario
+                setBusquedaLetra={setBusquedaLetra}
+            />
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-6">
+                        <Info info={info} />
+                    </div>
+                    <div className="col-md-6">
+                        <Cancion letra={letra} />
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
---
+++ src/components/Info.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Info = ({ info }) => {
+
+    if (Object.keys(info).length === 0) return null;
+
+
+    return (
+        <div className="card border-light">
+            <div className="card-header bg-primary text-light font-weight-bold">
+                Información Artista
+            </div>
+            <div className="card-body">
+                <img src={info.strArtistThumb} alt="Logo Artista" />
+                <p className="card-text">Género: {info.strGenre}</p>
+                <h2 className="card-text">Biografía:</h2>
+                <p className="card-text">{info.strBiographyES}</p>
+                <p className="card-text">
+                    <a href={`https://${info.strFacebook}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-facebook"></i>
+                    </a>
+                    <a href={`https://${info.strTwitter}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href={`${info.strLastFMChart}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-lastfm"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Info;
